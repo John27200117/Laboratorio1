e15a95525bcec72c6bffca2ebf071abfba5ad7fb
John27200117 <122114472+John27200117@users.noreply.github.com>
2023-01-06 21:32:44 -0500

    Update ejercicio10.py

---
+++ ejercicio10.py
@@ -0,0 +1,3 @@
+#La importancia de la recursividad es que hace sencillo problemas complejos ya que reduce la cantidad de código.
+#además que hace que el programa se vea más limpio y ordenado.
+#La generación de secuencias es más fácil con la recursividad que con algunas iteraciones anidadas.
